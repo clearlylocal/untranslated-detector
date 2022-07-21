1f544ad6e2619d22dd14be45f79decc1355e004e
lionel-rowe <lionel.rowe@gmail.com>
2022-07-21 03:41:39 +0100

    Add bulk file handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,99 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useForm } from 'react-hook-form'
 import './App.css'
-import xlsx from 'xlsx'
 import { check } from './check'
-import { serializeCol } from './serializeCol'
+import ExcelJS from 'exceljs'
 
 const init = {
-	sn: 'Sheet1',
-	src: 'eng',
-	trg: 'bn-rBD',
-	cm: 'Note',
+	s: 9,
+	t: 10,
+	c: 11,
 	cmtmp: 'Same as English: {{words}}',
+	suf: '_untranslated_{{date}}',
 }
 
-const stored: typeof init =
-	JSON.parse(localStorage.getItem('form') ?? 'null') ?? init
+const stored: typeof init = {
+	...init,
+	...JSON.parse(localStorage.getItem('form') ?? '{}'),
+}
+
+const suffixize = (filename: string, suffix: string) => {
+	const [, pre, ext] = filename.match(/^(.+)(\.[^.]+)$/) ?? [
+		null,
+		filename,
+		'',
+	]
+
+	return (
+		pre +
+		suffix.replace(
+			'{{date}}',
+			new Date().toISOString().replace(/\D/g, ''),
+		) +
+		ext
+	)
+}
+
+const parseExcelFile = async (
+	files: FileList,
+	commentTemplate: string,
+	suffix: string,
+	cols: [number, number, number],
+) => {
+	if (!files.length) return
+
+	const Col = {
+		Src: cols[0],
+		Trg: cols[1],
+		Comments: cols[2],
+	}
+
+	for (const file of files) {
+		const reader = new FileReader()
+
+		reader.onloadend = (_event) => {
+			const arrayBuffer = reader.result as ArrayBuffer
+
+			const workbook = new ExcelJS.Workbook()
+
+			workbook.xlsx.load(arrayBuffer).then(async (workbook) => {
+				workbook.worksheets.forEach((sheet) => {
+					sheet.eachRow((row, _rowNumber) => {
+						const omitted = check(
+							row.values[Col.Src] ?? '',
+							row.values[Col.Trg] ?? '',
+							row.values[Col.Comments] ?? '',
+						)
+
+						if (omitted.length) {
+							row.getCell(Col.Comments).value = [
+								row.values[Col.Comments],
+								commentTemplate.replace(
+									'{{words}}',
+									omitted.map((x) => `“${x}”`).join(', '),
+								),
+							]
+								.filter(Boolean)
+								.join('\n---\n')
+						}
+					})
+				})
+
+				const buf = await workbook.xlsx.writeBuffer({})
+
+				const a = document.createElement('a')
+				const outFile = new Blob([buf], {
+					type: 'image/jpeg',
+				})
+				a.href = URL.createObjectURL(outFile)
+				a.download = suffixize(file.name, suffix)
+				a.click()
+			})
+		}
+
+		reader.readAsArrayBuffer(file)
+	}
+}
 
 export const App = () => {
 	const { register, watch, handleSubmit } = useForm()
@@ -23,151 +102,25 @@ export const App = () => {
 		localStorage.setItem('form', JSON.stringify(data))
 	}
 
-	const sheetName: string = watch('sn') ?? stored.sn
-
-	const srcColHeading: string = watch('src') ?? stored.src
-	const trgColHeading: string = watch('trg') ?? stored.trg
-	const commentsColHeading: string = watch('cm') ?? stored.cm
-
 	const commentTemplate: string = watch('cmtmp') ?? stored.cmtmp
+	const filenameSuffix: string = watch('suf') ?? stored.suf
+
+	const s: number = Number(watch('s') ?? stored.s)
+	const t: number = Number(watch('t') ?? stored.t)
+	const c: number = Number(watch('c') ?? stored.c)
 
 	const files: FileList = useMemo(
 		() => watch('file') ?? ([] as any as FileList),
 		[watch],
 	)
 
-	const [workbook, setWorkbook] = useState<xlsx.WorkBook | null>(null)
-
 	useEffect(() => {
-		if (!files.length) return
-
-		const file = files[0]
-		const reader = new FileReader()
-		reader.onload = e => {
-			const data = new Uint8Array(e.target.result as ArrayBuffer)
-			const workbook = xlsx.read(data, { type: 'array' })
-
-			setWorkbook(workbook)
-		}
-
-		reader.readAsArrayBuffer(file)
-	}, [files])
-
-	const rows = useMemo(() => {
-		if (!workbook?.Sheets?.[sheetName]) {
-			return []
-		}
-
-		const rows = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName], {})
-
-		return rows
-	}, [workbook, sheetName]) as Record<string, string>[]
-
-	const parsed = useMemo(() => {
-		if (!workbook) {
-			return new Error('Upload a file')
-		}
-
-		const cloned = JSON.parse(JSON.stringify(rows)) as typeof rows
-
-		try {
-			const headers = xlsx.utils.sheet_to_json(
-				workbook.Sheets[sheetName],
-				{ header: 1 },
-			)[0] as string[]
-
-			if (!headers?.length) {
-				return new Error('Sheet not found or has no headers')
-			}
-
-			if (
-				[srcColHeading, trgColHeading, commentsColHeading].some(
-					name => !headers.includes(name),
-				)
-			) {
-				return new Error('Not all headers found')
-			}
-
-			return cloned.map(row => {
-				const omitted = check(
-					row[srcColHeading] ?? '',
-					row[trgColHeading] ?? '',
-					row[commentsColHeading] ?? '',
-				)
-
-				if (omitted.length) {
-					row[commentsColHeading] = [
-						row[commentsColHeading],
-						commentTemplate.replace(
-							'{{words}}',
-							omitted.map(x => `“${x}”`).join(', '),
-						),
-					]
-						.filter(Boolean)
-						.join('\n---\n')
-				}
-
-				return row
-			})
-		} catch (e) {
-			console.error(e)
-
-			return e as Error
-		}
-	}, [
-		workbook,
-		rows,
-		srcColHeading,
-		trgColHeading,
-		commentsColHeading,
-		commentTemplate,
-		sheetName,
-	])
-
-	;(window as any).parsed = parsed
+		parseExcelFile(files, commentTemplate, filenameSuffix, [s, t, c])
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [files /* commentTemplate, filenameSuffix */])
 
 	return (
 		<form onChange={handleSubmit(changeHandler)}>
-			<label>
-				Sheet name
-				<input
-					name='sn'
-					defaultValue={stored.sn}
-					ref={register}
-				></input>
-			</label>
-			<br />
-			<br />
-			<label>
-				Source col heading
-				<input
-					name='src'
-					defaultValue={stored.src}
-					ref={register}
-				></input>
-			</label>
-			<br />
-			<br />
-			<label>
-				Target col heading
-				<input
-					name='trg'
-					defaultValue={stored.trg}
-					ref={register}
-				></input>
-			</label>
-			<br />
-			<br />
-			<label>
-				Comments col heading
-				<input
-					name='cm'
-					defaultValue={stored.cm}
-					ref={register}
-				></input>
-			</label>
-			<br />
-			<br />
 			<label>
 				Comments template
 				<input
@@ -179,24 +132,38 @@ export const App = () => {
 			<br />
 			<br />
 			<label>
-				Upload file (XLSX)
-				<input type='file' name='file' ref={register} />
+				Output filename suffix
+				<input name='suf' defaultValue={stored.suf} ref={register} />
 			</label>
 			<br />
 			<br />
-			<hr />
-			<br />
-			{parsed instanceof Error ? (
-				<strong>{parsed.message}</strong>
-			) : (
-				<textarea
-					readOnly
-					value={serializeCol([
-						commentsColHeading,
-						...parsed.map(p => p[commentsColHeading] ?? ''),
-					])}
+			<label>
+				Column numbers
+				<input
+					type='number'
+					name='s'
+					defaultValue={stored.s}
+					ref={register}
 				/>
-			)}
+				<input
+					type='number'
+					name='t'
+					defaultValue={stored.t}
+					ref={register}
+				/>
+				<input
+					type='number'
+					name='c'
+					defaultValue={stored.c}
+					ref={register}
+				/>
+			</label>
+			<br />
+			<br />
+			<label>
+				Upload file (XLSX)
+				<input type='file' name='file' multiple ref={register} />
+			</label>
 		</form>
 	)
 }
--- src/serializeCol.tsx
+++
@@ -1,7 +0,0 @@
-export function serializeCol(col: string[]) {
-	return col
-		.map(cell =>
-			/[\t\n"]/.test(cell) ? `"${cell.split('"').join('""')}"` : cell,
-		)
-		.join('\n')
-}
